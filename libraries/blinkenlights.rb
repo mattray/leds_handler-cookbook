e04e5250a5eea2be245bfb5b0dbba831aebe2fec
Matt Ray <matthewhray@gmail.com>
2019-11-14 16:27:19 +1100

    tested and working with a BeagleBone Black

--- libraries/blinkenlights.rb
+++ libraries/blinkenlights.rb
@@ -2,18 +2,36 @@ require 'mixlib/shellout'
 
 module HandlerLED
   class Blinkenlights
-
-    def lights_flashing()
-      Mixlib::ShellOut.new("echo FLASHING > /tmp/lights_flashing_start").run_command
+    def lights_flashing
+      stdout = Mixlib::ShellOut.new('ls /sys/devices/platform/leds/leds').run_command.stdout
+      unless stdout.nil?
+        leds = stdout.split
+        sleep = 1.0 / leds.length
+        leds.each do |led|
+          Mixlib::ShellOut.new("echo heartbeat > /sys/devices/platform/leds/leds/#{led}/trigger").run_command
+          Mixlib::ShellOut.new("sleep #{sleep}").run_command
+        end
+      end
     end
 
-    def lights_on()
-      Mixlib::ShellOut.new("echo ON > /tmp/lights_on_failed").run_command
+    def lights_on
+      stdout = Mixlib::ShellOut.new('ls /sys/devices/platform/leds/leds').run_command.stdout
+      unless stdout.nil?
+        leds = stdout.split
+        leds.each do |led|
+          Mixlib::ShellOut.new("echo default-on > /sys/devices/platform/leds/leds/#{led}/trigger").run_command
+        end
+      end
     end
 
-    def lights_off()
-      Mixlib::ShellOut.new("echo OFF > /tmp/lights_off_succeeded").run_command
+    def lights_off
+      stdout = Mixlib::ShellOut.new('ls /sys/devices/platform/leds/leds').run_command.stdout
+      unless stdout.nil?
+        leds = stdout.split
+        leds.each do |led|
+          Mixlib::ShellOut.new("echo none > /sys/devices/platform/leds/leds/#{led}/trigger").run_command
+        end
+      end
     end
-
   end
 end
